994b983a83c749bf7dd58e0ea61a5f44e8390bd4
Tash <tsukimi.ukiyo@gmail.com>
2025-08-08 23:15:52 +0200

    created a handle client function that is a static method and extends from conn which is a net.Conn object from a struct clientConn I can now extract the request, uri and ive created a map for html paths

--- main.go
+++ main.go
@@ -3,7 +3,24 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
+
+
+
+var routes = map[string]string{
+	"/index": "hello",
+}
+
+type HTMLREQ struct {
+	requestType string
+	requested string
+	route string
+}
+
+type clientConn struct {
+	net.Conn
+}
 
 const message string = "Hello"
 
@@ -16,18 +33,44 @@ func main() {
 		fmt.Printf("Connection from %s\n", conn.RemoteAddr())
 	
 		log(connerr)
+		clientConn := &clientConn{conn}
 		go func() {
-		buffer := make([]byte, 1024)
-		data, readErr := conn.Read(buffer)
-		fmt.Printf("%s\n",string(buffer[:data]))
-		log(readErr)
-		defer conn.Close()
-		conn.Write([]byte(message))
+			clientConn.handleClient()
+			
 		}()
 	}
 	
 		
 }
+
+func (conn *clientConn) handleClient(){
+	var(
+		method string
+		uri string
+	)
+	buffer := make([]byte, 1024)
+	data, readErr := conn.Read(buffer)
+	stringData := string(buffer[:data])
+	fmt.Printf("%s\n",stringData)
+	log(readErr)
+	
+	//quoted := strconv.Quote(stringData)
+	//var slash string = `/`
+	request := strings.Split(stringData, "\r")[0]
+	method = strings.Split(request, "/")[0]
+	uri = strings.Split(stringData, "\r")[1]
+	fmt.Printf("Request: %v\n", request)
+	fmt.Printf("Method: %s\n", method)
+	fmt.Printf("Uri: %s\n", uri)
+	if (method == "GET"){
+		fmt.Printf("Get request logged: %s\n", uri)
+	}
+	//fmt.Printf("[+] Raw string: %s\n", quoted)
+	defer conn.Close()
+	//conn.Write([]byte(message))
+}
+
+
 
 func log(err error) {
 	if err != nil {
